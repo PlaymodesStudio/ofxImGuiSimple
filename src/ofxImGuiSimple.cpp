4b042a51aa526871f64801664b03b0858dc0ed0a
Eduard Frigola <eduardfrigola@gmail.com>
2023-07-20 12:56:51 +0200

    Disable Keyboard navigation by default

--- src/ofxImGuiSimple.cpp
+++ src/ofxImGuiSimple.cpp
@@ -21,7 +21,7 @@ void ofxImGuiSimple::setup(ofAppBaseWindow* win){
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
